f633ec60c0d26e935addf16dccdd0a8f4b0c2c68
Jason Perlman <jasonpperlman@gmail.com>
2019-04-04 10:46:24 -0400

    slowing down pace of gradient

--- src/App.vue
+++ src/App.vue
@@ -61,7 +61,7 @@ body {
   color: #fff;
   background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
   background-size: 400% 400%;
-  animation: Gradient 15s ease infinite;
+  animation: Gradient 30s ease infinite;
 }
 
 @keyframes Gradient {
@@ -75,6 +75,23 @@ body {
     background-position: 0% 50%;
   }
 }
+/* @keyframes Gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  40% {
+    background-position: 10% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  90% {
+    background-position: 90% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+} */
 #linguisti-container {
   display: flex;
   flex-direction: column;
